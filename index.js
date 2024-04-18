549371b4b8740a57ca7f724c073596a2ec5240ea
HJska27 <kallvey.thong23@student.cart.org>
2024-04-18 10:41:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function checkWinner() {
      for (let r = 0; r < rows; r++) {
          for (let c = 0; c < columns - 3; c++){
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]) {
+                if (board[r][c] == board[r][c+2] && board[r][c+2] == board[r][c+3] && board[r][c+3] == board[r][c+4]) {
                     setWinner(r, c);
                     return;
                 }
@@ -81,7 +81,7 @@ function checkWinner() {
     for (let c = 0; c < columns; c++) {
         for (let r = 0; r < rows - 3; r++) {
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+2][c] && board[r+2][c] == board[r+3][c]) {
+                if (board[r][c] == board[r+2][c] && board[r+2][c] == board[r+3][c] && board[r+3][c] == board[r+4][c]) {
                     setWinner(r, c);
                     return;
                 }
@@ -93,7 +93,7 @@ function checkWinner() {
     for (let r = 0; r < rows - 3; r++) {
         for (let c = 0; c < columns - 3; c++) {
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r+1][c+1] && board[r+1][c+1] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3]) {
+                if (board[r][c] == board[r+2][c+2] && board[r+2][c+2] == board[r+3][c+3] && board[r+3][c+3] == board[r+4][c+4]) {
                     setWinner(r, c);
                     return;
                 }
@@ -105,7 +105,7 @@ function checkWinner() {
     for (let r = 3; r < rows; r++) {
         for (let c = 0; c < columns - 3; c++) {
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r-1][c+1] && board[r-1][c+1] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3]) {
+                if (board[r][c] == board[r-2][c+2] && board[r-2][c+2] == board[r-3][c+3] && board[r-3][c+3] == board[r-4][c+4]) {
                     setWinner(r, c);
                     return;
                 }
