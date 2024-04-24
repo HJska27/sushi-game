3697d8a8b60e13116b3e92128f0e73af506757df
HJska27 <kallvey.thong23@student.cart.org>
2024-04-24 08:40:48 -0700

    da

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function checkWinner() {
      for (let r = 0; r < rows; r++) {
          for (let c = 0; c < columns - 3; c++){
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r][c+2] && board[r][c+2] == board[r][c+3] && board[r][c+3] == board[r][c+4]) {
+                if (board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+3] && board[r][c+3] == board[r][c+4]) {
                     setWinner(r, c);
                     console.log('horizontal')
                     return;
@@ -82,7 +82,7 @@ function checkWinner() {
     for (let c = 0; c < columns; c++) {
         for (let r = 0; r < rows - 3; r++) {
             if (board[r][c] != ' ') {
-                if (board[r][c] == board[r+2][c] && board[r+2][c] == board[r+3][c] && board[r+3][c] == board[r+4][c]) {
+                if (board[r][c] == board[r+1][c] && board[r+1][c] == board[r+3][c] && board[r+3][c] == board[r+4][c]) {
                     setWinner(r, c);
                     console.log('vertical')
                     return;
